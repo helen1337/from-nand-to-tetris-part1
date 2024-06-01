5b62c25142f50427af96779fa32ee0c51f46b515
Elena <lenchik982513@gmail.com>
2024-06-01 06:03:05 -0600

    add pro4,5 + image pack

---
+++ project4/mult/Mult.asm
@@ -0,0 +1,26 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+//// Replace this comment with your code.
+@R2
+M=0
+(LOOP)
+@R1
+MD=M-1
+@END
+D;JLT
+@R0
+D=M
+@R2
+M=D+M
+@LOOP
+0;JMP
+(END)
+@END
+0;JMP
